130a07769e1199238261ec7c944ab8c52d19b22e
Sai Kranthi <iamsaikranthi@gmail.com>
2021-11-10 02:12:12 +0530

    set Input value empty on sucess
    set Input value empty on sucess

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ const App = () => {
         },
       });
       console.log("GIF sucesfully sent to program", inputValue);
-
+      setInputValue("")
       await getGifList();
     } catch (error) {
       console.log("Error sending GIF:", error);
